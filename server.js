d94da9b97482a976536365d4f891f0f07b69a3e2
kanwisher <kanwisher@gmail.com>
2018-03-15 23:40:12 -0400

    add fetch and a basic route

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
+const code = "123456" //generateRandom();
+
+function generateRandom() {
+  var codeArray = [];
+  for(var i = 0; i < 6; i++){
+    codeArray.push(Math.floor(Math.random() * 10));  
+  }
+  return codeArray.join();
+}
  
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.get('/attempt', function (req, res) {
-    console.log(req.query.code);
+    var userGuess = req.query.code;
+    if(userGuess === code){
+      res.send("correct");
+    } else {
+      res.send("incorrect");
+    }
 });
  
 app.listen(3000);
